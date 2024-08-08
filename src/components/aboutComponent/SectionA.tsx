24f6c112a89892933422907084882d7900e5db99
ekadaegile <egileoniso.ekada@gmail.com>
2024-08-08 15:14:36 +0100

    update

--- src/components/aboutComponent/SectionA.tsx
+++ src/components/aboutComponent/SectionA.tsx
@@ -3,7 +3,8 @@
 export default function SectionA() {
     return (
         <div className=' pb-24 lg:pb-32 pt-40 lg:pt-48 w-full relative text-center text-white ' >
-            <p className=' paytone-one-regular text-3xl lg:text-[57px] lg:leading-[65px] relative z-10 font-black ' >“The Platform for Cause-Driven Events”</p>
+        <p className=' text-3xl xl:text-[56px] lg:text-[45px] xl:leading-[60px] lg:leading-[50px] relative z-10 ' >Why Hiroek</p>
+            <p className=' text-xl xl:text-[50px] relative z-10 mt-3  ' >“The Platform for Cause-Driven Events”</p>
             <img alt="aboutbg" src="/images/aboutbg.png" className=" absolute inset-0 w-full h-full object-cover " />
         </div>
     )
--- src/components/aboutComponent/SectionB.tsx
+++ src/components/aboutComponent/SectionB.tsx
@@ -3,14 +3,22 @@
 export default function SectionB() {
     return (
         <div className=' w-full py-14 flex lg:flex-row flex-col relative lg:gap-0 gap-6 items-center px-6 lg:px-12 bg-[#F0F2FF] justify-center text-[#37137F] ' >
-            <div className=' lg:max-w-[500px] font-black relative z-10 flex flex-col gap-3 text-lg lg:text-[38px] lg:leading-[57px] ' >
+            {/* <div className=' lg:max-w-[500px] font-black relative z-10 flex flex-col gap-3 text-lg lg:text-[38px] lg:leading-[57px] ' >
                 <p className=" text-3xl lg:text-[40px] lg:leading-[50px] " >Why Hiroek?</p>
                 <p  >Hiroek is on a mission to unlock the good in everyone, driven by humanity's potential to create positive change.</p>
-            </div>
-            <div className=" lg:max-w-[700px] font-medium lg:text-[26px] relative z-10 lg:leading-[39px] flex flex-col gap-4 font-axiformamedium " >
-                <p className="  " >Hiroek is the platform for fundraisers, donors and charities. We provide the tools that give fundraisers the edge and drive greater impact for the causes you care about.</p>
-                <p>{`We’re empowering a new generation of philanthropists, supporters and action-takers. with a mobile first platform making It easier for you to come together with your peers to support causes you care about.`}</p>
-                <p>{`An app for positive peer to peer support. A tool for grass root based organisation to create and expand their spaces to amplify the crucial role they play in advancing social well-being.`}</p>
+            </div> */}
+            <div className="  font-medium lg:text-[26px] relative z-10 lg:leading-[39px] flex flex-col gap-4  " >
+                {/* <p className="text-3xl lg:text-[40px] lg:leading-[50px] text-center " >Why Hiroek</p> */}
+                <p className=" whitespace-pre-line font-axiformamedium " >{`
+                Hiroek is designed to bring people together around causes they care about. Whether it's friends, work colleagues, or community groups, Hiroek makes it easy to create, manage, and participate in events to support various charitable causes.  \n
+                A dedicated mobile platform making It easier for you to come together with your peers to support causes you care about. \n
+                Empowering a new generation of philanthropists, supporters and action- takers (Uniting thinkers, carers and doers) \n
+                A tool for grass root based organisation to create and expand their spaces to amplify the crucial role they play in advancing social well-being.  \n
+                Hiroek is not just a tool for organising events but also a platform for building a community around shared causes. We aim to empower individuals and groups to make a meaningful impact.
+                
+                `}</p>
+                {/* <p>{`We’re empowering a new generation of philanthropists, supporters and action-takers. with a mobile first platform making It easier for you to come together with your peers to support causes you care about.`}</p>
+                <p>{`An app for positive peer to peer support. A tool for grass root based organisation to create and expand their spaces to amplify the crucial role they play in advancing social well-being.`}</p> */}
             </div>
             <img alt="hand" src="/images/hand.png" className=" absolute bottom-0 left-[9%] opacity-5 object-cover " />
         </div>
