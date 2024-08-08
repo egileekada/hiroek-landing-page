355346e29ffd43383c0b425063305298eeb35334
ekadaegile <egileoniso.ekada@gmail.com>
2024-08-08 12:41:15 +0100

    update

--- src/components/aboutComponent/SectionC.tsx
+++ src/components/aboutComponent/SectionC.tsx
@@ -5,7 +5,7 @@ import { AppleStore, Forwardarrowcolored, GooglePlay } from '../svg'
 export default function SectionC() {
     return (
         <div className=' py-20 relative text-[#F0F2FF] px-6 lg:px-12 flex w-full gap-3 items-center flex-col ' >
-            <p className=' text-xl lg:text-[38px] lg:leading-[57px] font-black text-center lg:max-w-[1115px] font-axiformamedium ' >Giving has long been underrated and underserved, so we're developing technology that makes a difference. Elevate your fundraising efforts and sign up now.</p>
+            <p className=' text-xl lg:text-[38px] lg:leading-[57px] font-black text-center lg:max-w-[1115px] font-axiformamedium ' >Be on the forefront of changing what it means to be a hero. Empower your inner hero by helping enforce changes where it really matters.</p>
             <CustomButton style={{ boxShadow: "3px 3px 0px 0px #37137F80" }} text="Get Started" size={"4"} type="button" className=" !bg-white !text-[#37137F] !font-black !w-fit !shadow-lg " icon={
                 <Forwardarrowcolored />
             } />
