b04cb35713d2ee7c1227531cfd4c4d52568f8726
egileekada <egileoniso.ekada@gmail.com>
2025-06-06 10:38:05 +0100

    update on ui

--- src/components/charityComponent/carousel.tsx
+++ src/components/charityComponent/carousel.tsx
@@ -83,7 +83,7 @@ export default function Carousel() {
                     {charityData?.map((item, index) => {
                         if (index === isShown) {
                             return (
-                                <div key={index} className=" flex flex-col lg:text-right top-0 lg:items-end h-[350px] lg:max-w-[80%] gap-6 " >
+                                <div key={index} className=" flex flex-col lg:text-right top-0 lg:items-end h-[230px] lg:h-[350px] lg:max-w-[80%] gap-6 " >
                                     <motion.p {...boxAnimation} className=" text-3xl whitespace-pre-line lg:text-[50px] lg:leading-[60px] uppercase " >{item?.name}</motion.p>
                                     <motion.p {...boxAnimation} className=" whitespace-pre-line text-[#424242] lg:text-2xl lg:leading-[36px] " >{item?.detail}</motion.p>
                                 </div>
