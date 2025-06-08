18bc6f91ff145e7ce1dbc1105164da232ad74ff8
egileekada <egileoniso.ekada@gmail.com>
2025-06-08 08:04:42 +0100

    update on ui

--- src/components/constant/index.ts
+++ src/components/constant/index.ts
@@ -48,7 +48,7 @@ export const eventData = [
     },
     {
         name: "Enhance User Experience",
-        detail: "Simplified registration and Streamline sign-up processes within the app reduce friction and improve the registration experience.",
+        detail: "Simplify registration and Streamline sign-up processes within the app reduce friction and improve the registration experience.",
         img: "/images/c2.png"
     },
     {
