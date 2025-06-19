88ee70b2ffd9650c7b1766c392fe80dc8877d758
egileekada <egileoniso.ekada@gmail.com>
2025-06-19 13:11:57 +0100

    update on ui

--- src/components/feeComponent/SectionA.tsx
+++ src/components/feeComponent/SectionA.tsx
@@ -15,8 +15,8 @@ export default function SectionA() {
                 <p className=' paytone-one-regular text-2xl lg:text-[40px] lg:leading-[120%] font-black relative z-10 ' >What Happens When I Buy a Ticket</p>
                 <p className=" text-lg lg:text-[24px] lg:leading-[120%] font-axiformamedium lg:font-axiformaheavy lg:max-w-[1282px] relative z-10 " >Buying tickets on Hiroek is quick and secure. We've partnered with Stripe to handle payments safely and efficiently.</p>
                 <p className=" text-lg lg:text-[24px] lg:leading-[120%] font-axiformamedium lg:font-axiformaheavy lg:max-w-[1282px] relative z-10 " >When you purchase a ticket, a small processing fee is applied:</p>
-                <p className=" text-lg lg:text-[24px] lg:leading-[120%] font-axiformamedium lg:font-axiformaheavy lg:max-w-[1282px] relative z-10 " >Stripe Fee: 1.5% + £0.20 per transaction</p>
-                <p className=" text-lg lg:text-[24px] lg:leading-[120%] font-axiformamedium lg:font-axiformaheavy lg:max-w-[1282px] relative z-10 " >Hiroek Fee: £0.60 per ticket</p>
+                <p className=" text-lg lg:text-[24px] lg:leading-[120%] font-axiformamedium lg:font-axiformaheavy lg:max-w-[1282px] relative z-10 " >Stripe Fee: 1.5% + £0.20p per transaction</p>
+                <p className=" text-lg lg:text-[24px] lg:leading-[120%] font-axiformamedium lg:font-axiformaheavy lg:max-w-[1282px] relative z-10 " >Hiroek Fee: £0.60p per ticket</p>
                 <p className=" text-lg lg:text-[24px] lg:leading-[120%] font-axiformamedium lg:font-axiformaheavy lg:max-w-[1282px] relative z-10 " >That's it - no hidden costs, Just a smooth experience from checkout to confirmation</p>
             </div>
 
